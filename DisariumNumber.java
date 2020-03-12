38b70759a5166147b0fd8e1fe1bc698c6cc958a2
Bharathi-Bala <61871018+Bharathi-Bala@users.noreply.github.com>
2020-03-12 16:47:24 +0530

    Add files via upload

---
+++ DisariumNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class DisariumNumber
+{
+	public static void main(String arg[])
+  {
+    Scanner input=new Scanner(System.in);
+    int number=input.nextInt();
+    String s=String.valueOf(number);
+    int temp=number;
+    double position=s.length();
+    int sum=0;
+    while(temp>0)
+    {
+      
+      double rem=temp%10;
+      double val=Math.pow(rem,position);
+      System.out.println(val);
+      sum+=val;
+      position-=1;
+      temp=temp/10;
+    }
+    if(sum==number)
+    {
+      System.out.println("It is a DisariumNumber ! ");
+    }
+    else
+    {
+      System.out.println("It is not a DisariumNumber ! ");
+    }
+  }
+}
